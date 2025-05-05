c68f95f9afb015b5e5535e9c2dae7d81a1c69f1b
vilbert03 <152169393+vilbert03@users.noreply.github.com>
2025-05-05 22:05:41 +0000

    database

--- castle_apartments/settings.py
+++ castle_apartments/settings.py
@@ -73,13 +73,23 @@ WSGI_APPLICATION = 'castle_apartments.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'verklegt_namskeid_db',
+        'USER': 'verklegt_db_user',
+        'PASSWORD': 'mfBckPbY3JzjOGvW878WnSKl',
+        'HOST': 'db-verklegt-namskeid-ii-eu-lpepbs.postgres.database.azure.com',
+        'PORT': '5432'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
